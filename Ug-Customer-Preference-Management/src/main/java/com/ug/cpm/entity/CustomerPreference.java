49b082357702af10d0138c3e840091f4c41c16a4
coder <coder@coder-vk00933780-ug-dev-workspace-vk>
2023-02-25 19:49:06 +0000

    Added booking history and Customer Personality Traits

--- Ug-Customer-Preference-Management/src/main/java/com/ug/cpm/entity/CustomerPreference.java
+++ Ug-Customer-Preference-Management/src/main/java/com/ug/cpm/entity/CustomerPreference.java
@@ -1,25 +1,24 @@
 package com.ug.cpm.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "T_CPM_CustomerPreference")
+@JsonIgnoreProperties(value = {"preferenceDescription"})
 public class CustomerPreference {
 
 	@Id
@@ -30,7 +29,7 @@ public class CustomerPreference {
 	@Column(name = "Customer_Firstname")
 	private String customerFirstName;
 	
-	@Column(name = "Customer_Id")
+	@Column(name = "Customer_Id", nullable = false)
 	private int customerId;
 	
 	@Column(name = "Customer_Lastname")
@@ -51,8 +50,8 @@ public class CustomerPreference {
 	@Column(name = "Preference_Name")
 	private String preferenceName;
 	
-	//@Transient
-	//private String preferenceDescription;
+	@Transient
+	private String preferenceDescription;
 	
 	@Column(name = "Preference_Createdon")
 	private String preferenceCreatedDate;
@@ -71,6 +70,8 @@ public class CustomerPreference {
 	
 	@Column(name = "Last_Updated_On")
 	private String updatedDate;
+	
+	
 
 	//@ManyToOne
 	//@JoinColumn(name = "Customer_Id", referencedColumnName = "Customer_Id")
--- Ug-Customer-Preference-Management/src/main/java/com/ug/cpm/service/NeomWebDataServiceImpl.java
+++ Ug-Customer-Preference-Management/src/main/java/com/ug/cpm/service/NeomWebDataServiceImpl.java
@@ -6,10 +6,14 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.ug.cpm.entity.Customer;
+import com.ug.cpm.entity.CustomerBookingHistory;
+import com.ug.cpm.entity.CustomerPersonalityTraits;
 import com.ug.cpm.entity.CustomerPreference;
 import com.ug.cpm.entity.Preference;
+import com.ug.cpm.repository.BookingHistoryRepository;
 import com.ug.cpm.repository.CustomerPreferenceRepository;
 import com.ug.cpm.repository.CustomerRepository;
+import com.ug.cpm.repository.PersonalityTraitsRepository;
 import com.ug.cpm.repository.PreferenceRepository;
 import com.ug.cpm.util.CustomerUtil;
 
@@ -20,22 +24,24 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @AllArgsConstructor
 @Slf4j
+@Transactional
 public class NeomWebDataServiceImpl implements NeomWebDataService {
 	
 	private CustomerRepository customerRepository;
-	
 	private CustomerPreferenceRepository customerPreferenceRepository;
-	
 	private PreferenceRepository preferenceRepository;
+	private PersonalityTraitsRepository personalityTraitsRepository;
+	private BookingHistoryRepository bookingRepository;
 
 	@Override
-	@Transactional
 	public void saveCustomerData(String customerData) throws Exception {
 		
 		log.info("In Save Customer Data service");
 		
 		Customer customer = CustomerUtil.parseCustomer(customerData);
 		List<CustomerPreference> customerPreferences = CustomerUtil.parseCustomerPreference(customerData, customer);
+		List<CustomerBookingHistory> customerBookingHistory = CustomerUtil.parseCustomerBookingHistory(customerData, customer);
+		CustomerPersonalityTraits customerPersonalityTraits = CustomerUtil.parseCustomerPersonalityTraits(customerData, customer);
 		
 		log.info("After parsing customer : "+ customer.toString());
 		
@@ -50,10 +56,13 @@ public class NeomWebDataServiceImpl implements NeomWebDataService {
 				preference.setPreferenceId(customerpref.getPreferenceId());
 				preference.setPreferenceType(customerpref.getPreferenceType());
 				preference.setPreferenceName(customerpref.getPreferenceName());
-				//preference.setPreferenceDescription(customerpref.getPreferenceDesciption());
+				//preference.setPreferenceDescription(customerpref.getPreferenceDescription());
 				preferenceRepository.save(preference);
 			}	
 		}
+		
+		personalityTraitsRepository.save(customerPersonalityTraits);
+		bookingRepository.saveAll(customerBookingHistory);
 		customerPreferenceRepository.saveAll(customerPreferences);
 	}
 
